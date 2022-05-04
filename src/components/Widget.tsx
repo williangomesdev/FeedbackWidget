2848434d42e0bfd71b33c724192efe268562102d
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-05-04 18:45:12 -0300

    Usando a biblioteca Popover da HeadlessUI para criar estados de abrir e fechar no widget e melhorar a acessibilidade

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,19 +1,15 @@
 import { ChatTeardropDots } from "phosphor-react";
-import { useState } from "react";
+import { Popover } from '@headlessui/react'
+
 
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
 
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen);
-  }
 
   return (
-    <div className="absolute bottom-5 right-5">
-      {isWidgetOpen && <p>Is Open</p>}
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Is Open</Popover.Panel>
 
-      <button
-        onClick={toggleWidgetVisibility}
+      <Popover.Button
         className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
       >
         <ChatTeardropDots className="w-6 h-6" />
@@ -21,7 +17,7 @@ export function Widget() {
           <span className="pl-2"></span>
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   );
 }
