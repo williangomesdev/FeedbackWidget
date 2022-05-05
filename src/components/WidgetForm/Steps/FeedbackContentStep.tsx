ac42b27826d6b894406a2a48470782e3ab6a4dee
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-05-05 15:36:55 -0300

    enviando dados do feedback apos o submit

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -14,8 +14,17 @@ export function FeedbackContentStep({
   onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState("");
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleSubmitFeedBack(event: FormEvent) {
+    event.preventDefault();
+    console.log({
+      screenshot,
+      comment,
+    });
+  }
 
   return (
     <>
@@ -39,25 +48,26 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <form className="mt-4 w-full">
+      <form onSubmit={handleSubmitFeedBack} className="mt-4 w-full">
         <textarea
+          onChange={(event) => setComment(event.target.value)}
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100  border-zinc-600 bg-transparent rounded-md resize-none focus:border-brand-500focus:ring-brand-500 focus:ring-1scrollbar-thumb-zinc-700scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
         />
-      </form>
 
-      <footer className=" flex gap-2 my-2 w-full">
-        <ScreenshotButton
-          screenshot={screenshot}
-          onScreenshotTook={setScreenshot}
-        />
-        <button
-          type="submit"
-          className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
-        >
-          Enviar feedback
-        </button>
-      </footer>
+        <footer className=" flex gap-2 my-2">
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
     </>
   );
 }
