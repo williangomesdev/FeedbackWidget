394337cd00d1674e41a3ff6d4526e5b1fbb7210e
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-05-05 02:19:01 -0300

    primeiro painel do feedback completo

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -3,6 +3,7 @@ import { CloseButton } from "./CloseButton";
 import bugImageUrl from "../assets/bug.svg";
 import ideaImageUrl from "../assets/lightbulb.svg";
 import thoughtImageUrl from "../assets/cloud.svg";
+import { useState } from "react";
 
 const feedbackTypes = {
   BUG: {
@@ -28,7 +29,11 @@ const feedbackTypes = {
   },
 };
 
+type feedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null);
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadown-lg w-[calc(100vw - 2rem)] md:w-auto">
       <header>
@@ -36,16 +41,23 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-          return (
-            <button key={key} className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none">
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          );
-        })}
-      </div>
+      {!feedbackType ? (
+        <div className="flex py-8 gap-2 w-full">
+          {Object.entries(feedbackTypes).map(([key, value]) => {
+            return (
+              <button
+                key={key}
+                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                type="button"
+                onClick={() => setFeedbackType(key as feedbackType)}
+              >
+                <img src={value.image.source} alt={value.image.alt} />
+                <span>{value.title}</span>
+              </button>
+            );
+          })}
+        </div>
+      ) : (<p>Relatando o problema do feedback</p>)}
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela{" "}
