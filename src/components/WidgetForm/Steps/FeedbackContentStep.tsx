0910cb385fe56aa06504554a0329c68fda823072
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-05-05 15:41:57 -0300

    desabilitar o botão enviar feedback caso a text area esteja com 0 caracteres

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -61,8 +61,9 @@ export function FeedbackContentStep({
             onScreenshotTook={setScreenshot}
           />
           <button
+            disabled={comment.length === 0}
             type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
             Enviar feedback
           </button>
