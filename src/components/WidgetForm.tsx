044f3eb08f5d7c350d12c428c29b0c46d02c152b
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-05-05 01:28:46 -0300

    Importando svg dos botões de widget

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,5 +1,11 @@
 import { CloseButton } from "./CloseButton";
 
+const feedbackTypes = {
+  BUG: {title:"Problema"},
+  IDEA: {title:"Ideia"},
+  OTHER: {title:"Outro"},
+};
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadown-lg w-[calc(100vw - 2rem)] md:w-auto">
@@ -7,7 +13,9 @@ export function WidgetForm() {
         <span className="text-xl leading-6">Deixe seu feedback</span>
         <CloseButton />
       </header>
-      <h1>Hello i'am a form</h1>
+
+      <div className="flex py-8 gap-2 w-full"></div>
+
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela{" "}
         <a
