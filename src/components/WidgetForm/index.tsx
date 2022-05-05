06c7c50ac7209f3529fc7378f2d1ef8a8707be62
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-05-05 12:49:23 -0300

    FeedbackType separado em uma função/

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -3,8 +3,9 @@ import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/lightbulb.svg";
 import thoughtImageUrl from "../../assets/cloud.svg";
 import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: "Problema",
     image: {
@@ -28,10 +29,10 @@ const feedbackTypes = {
   },
 };
 
-type feedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
-  const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null);
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadown-lg w-[calc(100vw - 2rem)] md:w-auto">
@@ -41,21 +42,7 @@ export function WidgetForm() {
       </header>
 
       {!feedbackType ? (
-        <div className="flex py-8 gap-2 w-full">
-          {Object.entries(feedbackTypes).map(([key, value]) => {
-            return (
-              <button
-                key={key}
-                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                type="button"
-                onClick={() => setFeedbackType(key as feedbackType)}
-              >
-                <img src={value.image.source} alt={value.image.alt} />
-                <span>{value.title}</span>
-              </button>
-            );
-          })}
-        </div>
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
         <p>Relatando o problema do feedback</p>
       )}
