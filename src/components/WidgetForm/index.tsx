bf691f148cb8ab71d03e83061666488bb8677ea2
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-05-05 16:13:03 -0300

    botão quero enviar outro funcionando

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -37,13 +37,16 @@ export function WidgetForm() {
   const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedback() {
+    setFeedbackSent(false);
     setFeedbackType(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw - 2rem)] md:w-auto">
       {feedbackSent ? (
-        <FeedbackSucessStep />
+        <FeedbackSucessStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
       ) : (
         <>
           {!feedbackType ? (
