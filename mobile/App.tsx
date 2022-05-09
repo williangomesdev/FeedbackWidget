902dfcba06a770d19110f3e3874e98c4528229d0
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-05-09 17:21:18 -0300

    criação de barra inferior --continuar em 49:11

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import AppLoading from "expo-app-loading";
@@ -6,7 +7,7 @@ import {
   Inter_400Regular,
   Inter_500Medium,
 } from "@expo-google-fonts/inter";
-import { Widget } from "./src/components/Widget";
+import Widget from "./src/components/Widget";
 import { theme } from "./src/theme";
 
 export default function App() {
@@ -26,8 +27,8 @@ export default function App() {
         backgroundColor: theme.colors.background,
       }}
     >
-      <Widget />
       <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Widget />
     </View>
   );
 }
